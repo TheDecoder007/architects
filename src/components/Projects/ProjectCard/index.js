bcedc56ec6b1df91634c2990f24f66e7e617fe2a
Ryan Murphy <TheDecoder007@gmail.com>
2023-01-25 14:08:24 -0500

    project card setup

---
+++ src/components/Projects/ProjectCard/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+
+function ProjectCard({
+    img, 
+    title,
+    description,
+    alt,
+}) {
+
+    return (
+
+
+    )
+}
+
+export default ProjectCard;
