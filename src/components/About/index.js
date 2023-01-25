4f48a49a5c9cb38a6646b73e6219b8206cce6119
Ryan Murphy <TheDecoder007@gmail.com>
2023-01-25 13:51:55 -0500

    initial components setup

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+import coverImage from "../../assets/photos/FacePic.png";
+
+
+const About = () => {
+
+    return (
+
+
+    )
+}
+
+
+
+export default About;
