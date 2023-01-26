68437eb9c9adf13ed9b4dcd51b9ad283857e4bd4
Ryan Murphy <TheDecoder007@gmail.com>
2023-01-26 12:27:05 -0500

    carousel and photos added

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,62 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel";
 
-const Hero = () => {
+import Residential1 from "../../assets/photos/residential/residence1.jpg";
+import Residential2 from "../../assets/photos/residential/residence2.jpg";
+import Residential3 from "../../assets/photos/residential/residence3.jpg";
+import Residential4 from "../../assets/photos/residential/residence4.jpg";
+import Residential5 from "../../assets/photos/residential/residence5.jpg";
+import Residential6 from "../../assets/photos/residential/residence6.jpg";
 
-    return (
+function Hero() {
+  return (
+    <Container fluid>
+      <Row>
+        <Carousel>
+            
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="../../assets/photos/residential/residence1.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-    );
-};
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+    </Container>
+  );
+}
 
 export default Hero;
