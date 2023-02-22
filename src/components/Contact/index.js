1cc17b8b155332d2907bcd11bdf376838a80a21d
Ryan Murphy <TheDecoder007@gmail.com>
2023-02-22 15:36:13 -0500

    added to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,18 +1,71 @@
 import React from "react";
 
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-
-//  const Contact = () => {
-
-//     return (
-
-//     );
-//  };
+import Card from "react-bootstrap/Card";
+import "./style.css";
+import Aos from "aos";
+import { Link } from "react-router-dom";
 
 
-//  export default Contact;
+
+
+ const Contact = () => {
+
+    return (
+<Container fluid className="mainCont">
+
+<Container fluid className="subCont">
+<Row>
+<h3 className="text-center sectionHead">
+        Our Resumes and Contact Information
+      </h3>
+</Row>
+</Container>
+
+<Row className="CardRow">
+    <Col className="colSolo">
+    <Card className="cardSolo">
+              <Card.Img
+                src={require("./../../assets/photos/Tom.jpg")}
+                className="images"
+              />
+              <Card.Body>
+                <Card.Title>Thomas F. Hibbard</Card.Title>
+                <Link to= "#tom">
+                <Button className="AllBtn">
+                  My Resume
+                </Button>
+                </Link>
+              </Card.Body>
+            </Card>
+    </Col>
+
+    <Col className="colSolo">
+    <Card className="cardSolo">
+              <Card.Img
+                src={require("./../../assets/photos/Carmelo.jpg")}
+                className="images"
+              />
+              <Card.Body>
+                <Card.Title>Carmelo Rosa</Card.Title>
+                <Link to= "#tom">
+                <Button className="AllBtn">
+                  My Resume
+                </Button>
+                </Link>
+              </Card.Body>
+            </Card>
+    </Col>
+
+</Row>
+
+
+</Container>
+    );
+ };
+
+
+ export default Contact;
