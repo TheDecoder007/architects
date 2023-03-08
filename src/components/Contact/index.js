11226b9206442bea921071015a196d8bc0b11f8e
Ryan Murphy <TheDecoder007@gmail.com>
2023-03-08 15:29:22 -0500

    fixed modals! started styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,14 +9,15 @@ import Card from "react-bootstrap/Card";
 import "./style.css";
 // import Aos from "aos";
 import { Link } from "react-router-dom";
-import ModalTom from "../ModalTom";
-import ModalCarm from "../ModalCarm";
 
 const Contact = () => {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [show2, setShow2] = useState(false);
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
 
   return (
     <Container fluid className="ContactCont">
@@ -54,28 +55,38 @@ const Contact = () => {
           </Card>
 
           {/* MODAL FOR RESUME */}
-          {/* <> */}
-            <Button src={ModalTom} onClick={handleShow}>My Resume</Button>
-            {/* <Modal show={show} onHide={handleClose}>
+          <>
+            <Button className="resButton" onClick={handleShow}>My Resume</Button>
+
+            <Modal show={show} onHide={handleClose}>
               <Modal.Header closeButton>
-                <Modal.Title>Thomas F. Hibbard Resume</Modal.Title>
+                <Modal.Title>Thomas F. Hibbard's Resume</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <p>
                   <h5>Experience</h5>
-                  Hibbard & Rosa Architects, L.L.C. 1996 - Present Middletown,
-                  CT Principal Seb J. Passanesi, P.C. 1992-1996 Middletown, CT
+                  Hibbard & Rosa Architects, L.L.C. 1996 - Present <br/>
+                  Middletown,
+                  CT 
+                  <br/>Principal Architect
+                  
+                  <br/><br/>Seb J. Passanesi, P.C. 1992-1996 Middletown, CT
                   Project Manager, Vice President
+                  <br/><br/>
                   <h5>Education</h5>
                   Catholic University of America Washington D.C. B.S.
                   Architecture 1981
+                  <br/><br/>
                   <h5>Memberships</h5>
-                  American Institute of Architects (AIA) International Code
-                  Council (ICC) GSA Registered Vendor
+                  American Institute of Architects (AIA) <br/>International Code
+                  Council (ICC) <br/>GSA Registered Vendor
+                  <br/><br/>
                   <h5>Licenses</h5>
                   State of Connecticut
+                  <br/><br/>
                   <h5>Military Experience</h5>
-                  U.S. Navy 1982-1986 U.S. Navy Reserves 1986-2004 Commander
+                  U.S. Navy 1982-1986 <br/>U.S. Navy Reserves 1986-2004 <br/>Commander
+                  <br/><br/>
                   <h5>Clients</h5>- Town of East Haven, CT - City of Middletown,
                   CT - Town of Suffield, CT - State of Connecticut; Department
                   of Public Works - Town of Windsor Locks, CT - City of New
@@ -94,8 +105,8 @@ const Contact = () => {
                   Close
                 </Button>
               </Modal.Footer>
-            </Modal> */}
-          {/* </> */}
+            </Modal>
+          </>
         </Col>
 
         <Col className="colSolo">
@@ -121,29 +132,33 @@ const Contact = () => {
           </Card>
 
           {/* MODAL FOR RESUME           */}
-          {/* <> */}
-          <Button src={ModalCarm} onClick={handleShow}>My Resume</Button>
+          <>
+            <Button className="resButton" onClick={handleShow2}>My Resume</Button>
 
-            {/* <Modal show={show} onHide={handleClose}>
+            <Modal show={show2} onHide={handleClose2}>
               <Modal.Header closeButton>
                 <Modal.Title>Carmelo Rosa's Resume</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <p>
                   <h5>Experience</h5>
-                  Hibbard & Rosa Architects, L.L.C. 1996 - Present Middletown,
-                  CT Principal Seb J. Passanesi, P.C. 1992-1996 Middletown, CT
-                  Project Manager
+                  Hibbard & Rosa Architects, L.L.C. 1996 - Present <br/>Middletown,
+                  CT <br/>Principal Architect<br/><br/>Seb J. Passanesi, P.C. 1992-1996 <br/>Middletown, CT
+                  <br/>Project Manager
+                  <br/><br/>
                   <h5>Education</h5>
                   Roger Williams University Bristol, R.I. B.S. Architecture 1987
-                  Minor in Civil Engineering
+                  <br/> Minor in Civil Engineering
+                  <br/><br/>
                   <h5>Memberships</h5>
-                  American Institute of Architects (AIA) International Code
-                  Council (ICC) GSA Registered Vendor Licensed in Twelve
+                  American Institute of Architects (AIA) <br/>International Code
+                  Council (ICC) <br/>GSA Registered Vendor<br/> Licensed in Twelve
                   Additional States
+                  <br/><br/>
                   <h5>Licenses</h5>
                   States of Connecticut, New York, Massachusetts, Rhode Island,
                   Vermont
+                  <br/><br/>
                   <h5>Clients</h5>- DBS, Inc. - O & G Industries - Chase
                   Enterprises, Inc. - Cornerstone Properties, Inc. - Borghesi
                   Building & Engineering, Inc. - City of Middletown, CT -
@@ -157,13 +172,12 @@ const Contact = () => {
                 </p>
               </Modal.Body>
               <Modal.Footer>
-                 <Button variant="secondary" onClick={handleClose}>
+                <Button variant="secondary" onClick={handleClose2}>
                   Close
-                </Button> 
+                </Button>
               </Modal.Footer>
-            </Modal> */}
-          {/* </> */}
-
+            </Modal>
+          </>
         </Col>
       </Row>
       <Row className="CardRow">
@@ -171,7 +185,7 @@ const Contact = () => {
           <h5>Company Info</h5>
 
           <h6>Hibbard & Rosa Architects, L.L.C.</h6>
-          <h6>100 Roverview Center, suite 272</h6>
+          <h6>100 Riverview Center, suite 272</h6>
           <h6>292 Main Street Middletown, CT 06457</h6>
           <h6>Tel: 860-346-1809</h6>
           <h6>Fax: 860-346-1809</h6>
